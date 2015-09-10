8fdc3a92a5331e1a4fb35fd49a35d03bae931f98
chn.challenger <chn.challenger@gmail.com>
2015-09-10 12:46:49 +0100

    ouside? method complete

--- lib/board.rb
+++ lib/board.rb
@@ -3,11 +3,59 @@ require_relative 'ship'
 
 
 class Board
-	def initialize 
+
+	def initialize(size=10)
+		@size = size 
 		@ships = []
 	end
 
+	def pending_ship_coords(ship, x, y, orientation)
+		result = []
+		if orientation == 'south'
+			(0..ship.size - 1).each do |i|
+				result  << [x,y]
+				x = x + 1
+			end
+		end
+		if orientation == 'north'
+			(0..ship.size - 1).each do |i|
+				result  << [x,y]
+				x = x - 1
+			end
+		end
+		if orientation == 'east'
+			(0..ship.size - 1).each do |i|
+				result  << [x,y]
+				y = y + 1
+			end
+		end
+		if orientation == 'west'
+			(0..ship.size - 1).each do |i|
+				result  << [x,y]
+				y = y - 1
+			end
+		end
+		return result
+	end
+
+	def outside?(ship, x, y, orientation)
+		pending_coords = pending_ship_coords(ship,x,y,orientation)
+		pending_coords = pending_coords.flatten
+		p pending_coords
+		pending_coords.each do |num|
+			if num < 0 || num >= @size
+				return true
+			end
+		end
+		return false
+	end
+
+
+
 	def place(ship, x, y, orientation)
+	outside = outside?(ship, x, y, orientation)
+	fail 'outside of grid' if outside == true
+#check outside??? if yes, then raise error
 
 		if orientation == 'south'
 			(0..ship.size - 1).each do |i|
@@ -50,7 +98,12 @@ class Board
 end 
 
 ship1 = Ship.new(5)
+ship2 = Ship.new(4)
 board1 = Board.new
+
+# board1.place(ship1, 1, 3, 'east')
+# board1.place(ship2,2,4,'west')
+# p board1
+board1.place(ship1,2,2,'west')
 p board1
-board1.place(ship1, 1, 3, 'east')
-p board1
+
